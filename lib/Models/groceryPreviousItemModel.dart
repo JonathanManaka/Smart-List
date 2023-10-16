d1df2cfc882f8cd2b1f836a44099c530edc2c2ba
JonathanManaka <mphomanaka91@gmail.com>
2023-10-16 13:57:33 +0200

    adding new splash screen

--- lib/Models/groceryPreviousItemModel.dart
+++ lib/Models/groceryPreviousItemModel.dart
@@ -1,12 +1,14 @@
 class GroceryPreviousItemModel {
   final String prevItemId;
   final double prevItemPrice;
+  final int prevItemQnty;
   final String prevItemCreatedDate;
   final String prevItemCalcFk;
 
   GroceryPreviousItemModel(
       {required this.prevItemId,
       required this.prevItemPrice,
+      required this.prevItemQnty,
       required this.prevItemCreatedDate,
       required this.prevItemCalcFk});
 }

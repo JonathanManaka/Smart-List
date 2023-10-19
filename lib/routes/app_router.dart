a78f365ac9865701687868f002f9b3ce0ab65b99
JonathanManaka <mphomanaka91@gmail.com>
2023-10-19 09:48:03 +0200

    initialized the splashScreenRoute

--- lib/routes/app_router.dart
+++ lib/routes/app_router.dart
@@ -4,6 +4,7 @@ import 'package:smart_grocery_list/views/HomePage.dart';
 import 'package:smart_grocery_list/views/addItemsPage.dart';
 import 'package:smart_grocery_list/views/groceryListStatsPage.dart';
 import 'package:smart_grocery_list/views/itemCalculatorPage.dart';
+import 'package:smart_grocery_list/views/splash_screen.dart';
 
 part 'app_router.gr.dart';
 
@@ -11,7 +12,8 @@ part 'app_router.gr.dart';
 class AppRouter extends _$AppRouter {
   @override
   List<AutoRoute> get routes => [
-        AutoRoute(page: HomeRoute.page, initial: true),
+        AutoRoute(page: SplashScreenRoute.page, initial: true),
+        AutoRoute(page: HomeRoute.page),
         AutoRoute(page: AddItemsRoute.page),
         AutoRoute(page: GroceryItemCalculatorRoute.page),
         AutoRoute(page: GroceryListStatsRoute.page),
